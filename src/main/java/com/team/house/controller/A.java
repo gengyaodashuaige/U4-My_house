6427c5cf45b2acf7487f8dc2dbbef09428116331
gengyao <773349304@.com>
2020-01-05 14:09:05 +0800

    改一改

--- src/main/java/com/team/house/controller/A.java
+++ src/main/java/com/team/house/controller/A.java
@@ -2,6 +2,6 @@ package com.team.house.controller;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("老板好！");
     }
 }
